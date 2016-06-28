e19638526192a1e903ebb10e724563f279da8d4a
Vincent Lee <dongho.vincent.lee@gmail.com>
2016-06-28 13:42:16 -0400

    subscriptions

---
+++ server/publications.js
@@ -0,0 +1,18 @@
+Meteor.publish("theHouses",
+	function(){return HouseDatas.find();})
+
+
+Meteor.publish("theComments",
+	function(){return Comments.find();})
+
+
+Meteor.publish("userData", function () {
+	if (this.userId) {
+		//return Meteor.users.find({});
+		return Meteor.users.find({_id: this.userId},
+			{fields:{profile:1, "services.google.email":1}});
+	} else {
+		this.ready();
+	}
+});
+
