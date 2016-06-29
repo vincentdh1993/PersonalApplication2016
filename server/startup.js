5a31279ed37a2aa611eed04a1193f850fd169d87
Vincent Lee <dongho.vincent.lee@gmail.com>
2016-06-29 13:35:13 -0400

    fixing

--- server/startup.js
+++ server/startup.js
@@ -2,6 +2,6 @@ Meteor.startup(function(){
 	HouseDatas.remove({});
 	HouseDatas.insert({address:"70 Hope Ave", city:"Waltham", state:"MA",deposit:1000, price:900, offeredBy:"Hyon"});
 	HouseDatas.insert({address:"60 Hope Ave", city:"Waltham", state:"MA",deposit:1000, price:900, offeredBy:"Sean"});
-	HouseDatas.insert({address:"17 shakespeare road", city:"Waltham", State: "MA",deposit:1000, price:700, offeredBy:"Jin"});
-	HouseDatas.insert({address:"415 South Street", city:"Waltham", State: "MA",deposit:1000, price: 1250, offeredBy:"Vincent"});
+	HouseDatas.insert({address:"17 shakespeare road", city:"Newton", State: "MA",deposit:1000, price:700, offeredBy:"Jin"});
+	HouseDatas.insert({address:"415 South Street", city:"Newton", State: "MA",deposit:1000, price: 1250, offeredBy:"Vincent"});
 })
