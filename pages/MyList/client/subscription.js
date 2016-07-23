6cb8ec8c0c48e26886022fecdd77f19c43e47630
Vincent Lee <dongho.vincent.lee@gmail.com>
2016-07-23 19:05:11 -0400

    heool

--- pages/MyList/client/subscription.js
+++
@@ -1,5 +0,0 @@
-//Meteor.subscribe("mylist");
-//Meteor.subscribe("mylist",Meteor.userId());
-//Meteor.subscribe("mylist",10);
-Meteor.subscribe("mylist",Meteor.userId());
-Meteor.subscribe("theHousesAt");
--- pages/ShowHouses/client/subscription.js
+++
@@ -1,5 +0,0 @@
-Meteor.subscribe("theHousesAt","Waltham");
-Meteor.subscribe("theHousesAt","Newton");
-Meteor.subscribe("theWishList");
-//Meteor.subscribe("theHousesBy","Dong Ho Lee");
-//Meteor.subscribe("theHouses");
